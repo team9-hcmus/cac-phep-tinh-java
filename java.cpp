743612c58f53298a8eb2fbcab629b6c12c67e404
team9-hcmus <75234933+team9-hcmus@users.noreply.github.com>
2020-12-07 18:17:17 +0700

    Create java.cpp

---
+++ java.cpp
@@ -0,0 +1,42 @@
+package cau_truc_switch_case;
+ 
+import java.util.Scanner;
+ 
+public class TinhToanVoi2So {
+ 
+    public static void main(String[] args) {
+        int aNumber, bNumber;
+        char kt;
+        Scanner scanner = new Scanner(System.in);
+     
+        System.out.println("Nhập vào số thứ nhất: ");
+        aNumber = scanner.nextInt();
+        System.out.println("Nhập vào số thứ hai: ");
+        bNumber = scanner.nextInt();
+        System.out.println("Nhập phép toán (+, -, *, /): ");
+        kt = scanner.next().charAt(0);
+         
+        switch (kt) {
+        case '+':
+            System.out.println(aNumber + " + " + bNumber + " = " + (aNumber + bNumber));
+            break;
+        case '-':
+            System.out.println(aNumber + " - " + bNumber + " = " + (aNumber - bNumber));
+            break;
+        case '*':
+            System.out.println(aNumber + " * " + bNumber + " = " + (aNumber * bNumber));
+            break;
+        case '/':
+            if (bNumber == 0) {
+                System.out.println("Số bị chia phải khác 0.");
+            } else {
+                // ép kiểu kết quả về double để có kết quả chính xác
+                System.out.println(aNumber + " / " + bNumber + " = " + (float) aNumber / bNumber);
+            }
+            break;
+        default:
+            System.out.println("Nhập phép tính không hợp lệ.");
+        }
+    }
+ 
+}
